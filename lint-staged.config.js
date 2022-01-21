27d771f1657440e68b0a2424a2abc7d3f587c1cc
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-21 15:49:36 +0900

    Chore: initial setting

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+
+  // Lint then format TypeScript and JavaScript files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `npx eslint --fix ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `npx prettier --write ${filenames.join(' ')}`,
+};
