28b81a7dd666a574e9dca1f88577768447260bc6
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-29 22:58:46 +0900

    [Chore] #1 common-ui StockList 컴포넌트의 css 클래스명을 bl_vertPosts에서 bl_vertStocks로 변경함

--- components/StockList/index.tsx
+++ components/StockList/index.tsx
@@ -7,21 +7,21 @@ const cx = classnames.bind(styles);
 
 const StockList = (): JSX.Element => {
   return (
-    <ul className={styles.bl_vertPosts}>
-      <li className={styles.bl_vertPosts_item}>
+    <ul className={styles.bl_vertStocks}>
+      <li className={styles.bl_vertStocks_item}>
         <Link href="/market/1">
-          <a className={styles.bl_vertPosts_link}>
-            <span className={styles.bl_vertPosts_stock}>
-              <p className={styles.bl_vertPosts_ttl}>코스피 지수</p>
-              <p className={styles.bl_vertPosts_standard}>28/01 | 서울</p>
+          <a className={styles.bl_vertStocks_link}>
+            <span className={styles.bl_vertStocks_stock}>
+              <p className={styles.bl_vertStocks_ttl}>코스피 지수</p>
+              <p className={styles.bl_vertStocks_standard}>28/01 | 서울</p>
             </span>
 
-            <span className={styles.bl_vertPosts_marketPrice}>
-              <p className={styles.bl_vertPosts_price}>2,663.34</p>
+            <span className={styles.bl_vertStocks_marketPrice}>
+              <p className={styles.bl_vertStocks_price}>2,663.34</p>
               <p
                 className={cx(
-                  styles.bl_vertPosts_fluctuation,
-                  styles.bl_vertPosts_fluctuation__red
+                  styles.bl_vertStocks_fluctuation,
+                  styles.bl_vertStocks_fluctuation__red
                 )}
               >
                 +48.85 (+1.87%)
