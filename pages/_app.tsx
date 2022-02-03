2c724f01a8ca7a66c78194aef0139064e8f050bd
2heeesss <hohooodo@gmail.com>
2022-02-03 12:27:29 +0900

    [Feat] #6 네비게이션 바 레이아웃

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
