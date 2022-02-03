8be0559e70fec50494bfdf6ec58cd4c584c7a25e
Serzhul <wilshere917@gmail.com>
2022-02-03 12:15:26 +0900

    [Feat] #9 Spinner Component 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
 import '../styles/globals.scss';
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
+import Spinner from '../components/ui/spinner';
+import { useState, useEffect, Fragment } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [isLoading, setIsLoading] = useState(false);
+
+  function routeChangeStart() {
+    Router.events.on('routeChangeStart', () => {
+      setIsLoading(true);
+    });
+  }
+
+  function routeChangeComplete() {
+    Router.events.on('routeChangeComplete', () => {
+      setIsLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    routeChangeStart();
+    routeChangeComplete();
+  });
+
+  return (
+    <Fragment>
+      {isLoading && <Spinner />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 export default MyApp;
