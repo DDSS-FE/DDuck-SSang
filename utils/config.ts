ec4da0e7af75b627529b67a8907f6ff0a3bb206e
Serzhul <wilshere917@gmail.com>
2022-02-15 11:07:51 +0900

    [Refactor] #25 Chart Style 변경사항 반영

--- utils/config.ts
+++ utils/config.ts
@@ -2,3 +2,4 @@ const SERVER = process.env.SERVER_API;
 
 export const CRYPTO_LINE_API = `${SERVER}/crypto/line`;
 export const CRYPTO_CANDLE_API = `${SERVER}/crypto/candle`;
+export const STOCK_CANDLE_API = `${SERVER}/stock/candle`;
