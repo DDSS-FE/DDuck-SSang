9a8e030f35bfe6e917c309d9e7c6d8d1fd379afd
Serzhul <wilshere917@gmail.com>
2022-02-17 17:14:05 +0900

    [Feat] KeyInfo Data Rendering

--- components/KeyInfo/index.tsx
+++ components/KeyInfo/index.tsx
@@ -1,40 +1,62 @@
 import styles from './KeyInfo.module.scss';
 
-const KeyInfo = (): JSX.Element => {
+import { MarketInfoProps } from '../../utils/market/types';
+
+import { formatDecimal } from '../../utils/formatDate';
+
+const KeyInfo = ({ data }: MarketInfoProps): JSX.Element => {
+  const {
+    summaryDetail: {
+      dayLow,
+      dayHigh,
+      fiftyTwoWeekLow,
+      fiftyTwoWeekHigh,
+      previousClose,
+      open,
+      volume,
+      averageVolume,
+    },
+  } = data;
   return (
     <ul className={styles.bl_keyInfo}>
       <li>
         <p>일일 변동폭</p>
-        <p>2591.53 ~ 2668.59</p>
+        <p>
+          {formatDecimal(dayLow)} ~ {formatDecimal(dayHigh)}
+        </p>
       </li>
       <li>
         <p>52주 가격 변동폭</p>
-        <p>591.53 ~ 668.59</p>
+        <p>
+          {formatDecimal(fiftyTwoWeekLow)} ~ {formatDecimal(fiftyTwoWeekHigh)}
+        </p>
       </li>
       <li>
         <p>이전 종가</p>
-        <p>91.53 ~ 168.59</p>
+        <p>{formatDecimal(previousClose)}</p>
       </li>
       <li>
         <p>시가</p>
-        <p>91.53 ~ 168.59</p>
+        <p>{formatDecimal(open)}</p>
       </li>
       <li>
         <p>거래량</p>
-        <p>91.53 ~ 168.59</p>
+        <p>{volume.toLocaleString()}</p>
       </li>
       <li>
         <p>평균 거래량</p>
-        <p>91.53 ~ 168.59</p>
+        <p>{averageVolume.toLocaleString()}</p>
       </li>
+      {/* 
+      이곳에 들어가는 데이터는 어디서 가져와야 할 지 논의 필요
       <li>
         <p># 구성종목</p>
         <p>91.53 ~ 168.59</p>
-      </li>
-      <li>
+      </li> */}
+      {/* <li>
         <p>1년 변동률</p>
         <p>91.53 ~ 168.59</p>
-      </li>
+      </li> */}
     </ul>
   );
 };
