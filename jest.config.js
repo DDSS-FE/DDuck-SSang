87813c37ba0a88996f7214cf5290f8c56d0fefd4
2heeesss <hohooodo@gmail.com>
2022-02-18 00:28:27 +0900

    [Bug] #40 경로 설정 정규식 수정

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,11 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    'components/(.*)$': '<rootDir>/components/$1',
-    'pages/(.*)$': '<rootDir>/pages/$1',
-    'utils/(.*)$': '<rootDir>/utils/$1',
-    'hooks/(.*)$': '<rootDir>/hooks/$1',
-    'styles/(.*)$': '<rootDir>/styles/$1',
+    '^components/(.*)$': '<rootDir>/components/$1',
+    '^pages/(.*)$': '<rootDir>/pages/$1',
+    '^utils/(.*)$': '<rootDir>/utils/$1',
+    '^hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^styles/(.*)$': '<rootDir>/styles/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
