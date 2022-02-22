ff007d43b4c8ccfd0a4e34381219930eca5729af
2heeesss <hohooodo@gmail.com>
2022-02-22 20:32:24 +0900

    [Feat] #35 add chart period button component

--- components/Chart/index.tsx
+++ components/Chart/index.tsx
@@ -1,17 +1,23 @@
 import dynamic from 'next/dynamic';
-import clsx from 'clsx';
 
+import { PeriodButton } from 'components/PeriodButton';
 import styles from 'components/Chart/Chart.module.scss';
 
 import { STOCK_CANDLE_API } from 'utils/config';
 import useAxios from 'hooks/useAxios';
+import { useState, useEffect } from 'react';
 
 const DynamicCanvas = dynamic(() => import('./ChartCanvas'), {
   ssr: false,
 });
 
 export function Chart(): JSX.Element {
-  const { data } = useAxios(STOCK_CANDLE_API);
+  const [period, setPeriod] = useState('D');
+  const { data, fetchData } = useAxios(STOCK_CANDLE_API, period);
+
+  useEffect(() => {
+    fetchData();
+  }, [period]);
 
   return (
     <div className={styles.ly_chart}>
@@ -19,17 +25,7 @@ export function Chart(): JSX.Element {
         {data ? <DynamicCanvas data={data} /> : null}
       </div>
 
-      <ul className={styles.bl_chartViewMenu}>
-        <li className={styles.bl_chartViewMenu_item}>15분</li>
-        <li className={styles.bl_chartViewMenu_item}>30분</li>
-        <li className={styles.bl_chartViewMenu_item}>60분</li>
-        <li className={clsx(styles.bl_chartViewMenu_item, styles.is_active)}>
-          1일
-        </li>
-        <li className={styles.bl_chartViewMenu_item}>1주</li>
-        <li className={styles.bl_chartViewMenu_item}>1달</li>
-        <li className={styles.bl_chartViewMenu_item}>1년</li>
-      </ul>
+      <PeriodButton callPeriod={setPeriod} />
     </div>
   );
 }
