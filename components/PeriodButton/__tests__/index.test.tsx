cbe4fa90408b256cd7336ca8cec9617fc1bd4328
2heeesss <hohooodo@gmail.com>
2022-02-22 20:33:02 +0900

    [Test] #35 add chart period button tests

---
+++ components/PeriodButton/__tests__/index.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+import { PeriodButton } from 'components/PeriodButton/index';
+import userEvent from '@testing-library/user-event';
+
+describe('Chart Period Button', () => {
+  const props = {
+    callPeriod: () => {},
+  };
+  describe('Layout', () => {
+    it.each`
+      period
+      ${'15분'}
+      ${'30분'}
+      ${'60분'}
+      ${'1일'}
+      ${'1주'}
+      ${'1달'}
+    `('has $period text', ({ period }) => {
+      render(<PeriodButton {...props} />);
+      const component = screen.getByTestId('PeriodButton-component');
+      expect(component).toHaveTextContent(period);
+    });
+  });
+  describe('Interactions', () => {
+    it('has is_active class name when init', () => {
+      render(<PeriodButton {...props} />);
+      const listitem = screen
+        .getAllByRole('listitem')
+        .find((li) => li.textContent === '1일');
+      expect(listitem).toHaveClass('is_active');
+    });
+    it.each`
+      listitemTxt
+      ${'15분'}
+      ${'30분'}
+      ${'60분'}
+      ${'1일'}
+      ${'1주'}
+      ${'1달'}
+    `('has not is_active class name when init', ({ listitemTxt }) => {
+      render(<PeriodButton {...props} />);
+      const listitem = screen
+        .getAllByRole('listitem')
+        .find((li) => li.textContent !== listitemTxt);
+      expect(listitem).not.toHaveClass('is_active');
+    });
+    it('has is_active class name when click 15분 button', () => {
+      render(<PeriodButton {...props} />);
+      const listitem = screen
+        .getAllByRole('listitem')
+        .find((li) => li.textContent === '15분');
+      userEvent.click(listitem);
+      expect(listitem).toHaveClass('is_active');
+    });
+  });
+});
