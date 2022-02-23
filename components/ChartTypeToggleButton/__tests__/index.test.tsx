ea542a99fe1300d7723cd45a7f9b4da6a8cd8f59
chmini <beelzebule@naver.com>
2022-02-23 16:28:57 +0900

    [Test] Add ChartTypeToggleButton test code

---
+++ components/ChartTypeToggleButton/__tests__/index.test.tsx
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction } from 'react';
+import { render, screen } from '@testing-library/react';
+
+import ChartTypeToggleButton, {
+  ChartType,
+} from 'components/ChartTypeToggleButton';
+
+describe('ChartTypeToggleButton 컴포넌트는', () => {
+  describe('Layout', () => {
+    it('chartType이 candle일 때 line 아이콘을 보여준다.', () => {
+      const setChartType: Dispatch<SetStateAction<ChartType>> = jest.fn();
+      render(
+        <ChartTypeToggleButton type="candle" setChartType={setChartType} />
+      );
+
+      const icon = screen.getByRole('img');
+      expect(icon).toHaveAttribute('alt', 'line');
+    });
+
+    it('chartType이 line일 때 candle 아이콘을 보여준다.', () => {
+      const setChartType: Dispatch<SetStateAction<ChartType>> = jest.fn();
+      render(<ChartTypeToggleButton type="line" setChartType={setChartType} />);
+
+      const icon = screen.getByRole('img');
+      expect(icon).toHaveAttribute('alt', 'candle');
+    });
+  });
+});
