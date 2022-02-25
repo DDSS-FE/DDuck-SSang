af25b98cb71f4258debdb13167c617998ec5e8e9
Serzhul <wilshere917@gmail.com>
2022-02-25 20:16:24 +0900

    [Refactor] #60 DetailNav applied common architecture.

--- components/DetailNav/index.tsx
+++ components/DetailNav/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
 import styles from 'components/DetailNav/DetailNav.module.scss';
 
@@ -9,23 +9,18 @@ export interface NavItem {
 }
 
 interface DetailNavProps {
-  items: NavItem[];
+  navItems: NavItem[];
 }
 
-const DetailNav = ({ items }: DetailNavProps): JSX.Element => {
-  // const router = useRouter();
+const DetailNav = ({ navItems }: DetailNavProps): JSX.Element => {
+  const router = useRouter();
 
   return (
     <nav className={styles.ly_header_nav} data-testid="DetailNav-component">
-      {items.map(({ name, href }: NavItem, index: number) => (
+      {navItems.map(({ name, href }: NavItem, index: number) => (
         <Link href={href} key={index}>
           <a className={styles.ly_header_nav_cat}>
-            <span
-            // ! : TODO. NavBar와 함께 next/router 학습 후 테스트 작성
-            // className={
-            // router.pathname === name && styles.active
-            // }
-            >
+            <span className={router.asPath === href ? styles.active : ''}>
               {name}
             </span>
           </a>
