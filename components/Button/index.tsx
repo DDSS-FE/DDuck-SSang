7c9b322210868585e6b290df09ac8660c3e11387
2heeesss <hohooodo@gmail.com>
2022-03-02 23:04:20 +0900

    [Feat] #75 add button icon

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,15 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from 'components/Button/Button.module.scss';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
   children: React.ReactNode;
   eventHandler: React.MouseEventHandler;
 }
-
 const Button = ({ children, eventHandler }: Props): JSX.Element => {
   return (
     <>
       <button onClick={eventHandler} className={styles.el_button}>
         {children}
+        <FontAwesomeIcon icon={faArrowRight} />
       </button>
     </>
   );
