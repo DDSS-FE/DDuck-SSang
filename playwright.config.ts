f94b1e3bad81b0307938b6356ca3d31fe20e908b
BonhaengLee <bon952014@ajou.ac.kr>
2022-03-05 23:19:08 +0900

    [Feat] Write test code for Watchlist & Add recoding files

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,28 @@
-import { PlaywrightTestConfig } from '@playwright/test';
-
+import { PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
+  workers: process.env.CI ? 2 : undefined,
+  retries: 2,
   use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
     launchOptions: {
-      slowMo: 500,
+      slowMo: 1000,
     },
+    video: 'on',
   },
+  projects: [
+    // {
+    //   name: 'Desktop Chromium',
+    //   use: {
+    //     browserName: 'chromium',
+    //   },
+    // },
+    // Test against mobile viewports.
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+  ],
 };
-export default config;
+
+module.exports = config;
--- tests/watchlist.spec.ts
+++ tests/watchlist.spec.ts
@@ -1,49 +1,91 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test.describe('Watchlist', () => {
+  test('관심목록 없음 확인 -> 시장 상세페이지에서 관심 목록 추가 -> 관심목록 확인 -> 삭제 -> 관심목록 확인', async ({
+    page,
+  }) => {
+    await page.goto('http://localhost:3000/');
+    // * : 로그인
+    await Promise.all([
+      page.waitForNavigation(/*{ url: 'http://localhost:3000/more' }*/),
+      page.locator('svg[role="img"]').nth(3).click(),
+    ]);
+    await page.locator('text=로그인').nth(2).click();
+    await page.locator('input[type="email"]').click();
+    await page.locator('input[type="email"]').fill('tester@crl.com');
+    await page.locator('input[type="email"]').press('Tab');
+    await page.locator('input[type="password"]').fill('123123');
+    await page.locator('button:has-text("Submit")').click();
 
-  await Promise.all([
-    page.waitForNavigation(/*{ url: 'http://localhost:3000/more' }*/),
-    page.locator('svg[role="img"]').nth(3).click(),
-  ]);
+    // * : 관심목록 탭 이동 후 관심 목록 없음 확인
+    await Promise.all([
+      await page.goto('http://localhost:3000/watchlist', {
+        waitUntil: 'domcontentloaded',
+      }),
+      page.locator('path').nth(2).click(),
+      await page.isVisible('bl_emptyData'),
+    ]);
 
-  // * : 로그인
-  await page.locator('text=로그인').nth(2).click();
-  await page.locator('input[type="email"]').click();
-  await page.locator('input[type="email"]').fill('tester@crl.com');
-  await page.locator('input[type="email"]').press('Tab');
-  await page.locator('input[type="password"]').fill('123123');
-  await page.locator('button:has-text("Submit")').click();
+    // * : 시장 탭 이동
+    await Promise.all([
+      page.waitForNavigation(/*{ url: 'http://localhost:3000/market/stock' }*/),
+      page.locator('text=시장뉴스관심목록더보기 >> path').first().click(),
+    ]);
 
-  // * : 관심목록 탭 이동 후 관심 목록 없음 확인
-  await Promise.all([
-    await page.goto('http://localhost:3000/watchlist', {
-      waitUntil: 'domcontentloaded',
-    }),
-    page.locator('path').nth(2).click(),
-    await page.isVisible('bl_emptyData'),
-  ]);
+    // * : 시장 상세 페이지에서 관심목록에 종목 추가
+    await page.goto('http://localhost:3000/market/stock/AAPL');
+    await page.locator('button >> nth=1').click();
 
-  // * : 시장 탭 이동
-  await Promise.all([
-    page.waitForNavigation(/*{ url: 'http://localhost:3000/market/stock' }*/),
-    page.locator('text=시장뉴스관심목록더보기 >> path').first().click(),
-  ]);
+    // // * : 관심목록 탭 이동
+    await page.locator('text=시장뉴스관심목록더보기 >> path').nth(2).click();
+    await expect(page).toHaveURL('http://localhost:3000/watchlist');
 
-  // * : 시장 상세 페이지에서 관심목록에 종목 추가
-  await page.goto('http://localhost:3000/market/stock/AAPL');
-  await page.locator('button >> nth=1').click();
+    // // * : 수정 버튼 클릭
+    await page.locator('button').click();
 
-  // // * : 관심목록 탭 이동
-  await page.locator('text=시장뉴스관심목록더보기 >> path').nth(2).click();
-  await expect(page).toHaveURL('http://localhost:3000/watchlist');
+    // // * : 삭제 버튼 클릭 후 관심 목록 없음 확인
+    await page.locator('button').nth(1).click();
+    const locator = page.locator('.StockList_bl_emptyData__4p4jN');
+    await expect(locator).toContainText('관심 목록이 없습니다.');
+  });
 
-  // // * : 수정 버튼 클릭
-  await page.locator('button').click();
+  test('시장 상세페이지에서 관심목록 추가/삭제 버튼 토글되는지 확인', async ({
+    page,
+  }) => {
+    await page.goto('http://localhost:3000/');
+    // * : 로그인
+    await Promise.all([
+      page.waitForNavigation(/*{ url: 'http://localhost:3000/more' }*/),
+      page.locator('svg[role="img"]').nth(3).click(),
+    ]);
+    await page.locator('text=로그인').nth(2).click();
+    await page.locator('input[type="email"]').click();
+    await page.locator('input[type="email"]').fill('tester@crl.com');
+    await page.locator('input[type="email"]').press('Tab');
+    await page.locator('input[type="password"]').fill('123123');
+    await page.locator('button:has-text("Submit")').click();
 
-  // // * : 삭제 버튼 클릭 후 관심 목록 없음 확인
-  await page.locator('button').nth(1).click();
-  await page.isVisible('bl_emptyData');
-  await page.pause();
+    // * : 시장 탭 이동
+    await Promise.all([
+      page.waitForNavigation(/*{ url: 'http://localhost:3000/market/stock' }*/),
+      page.locator('text=시장뉴스관심목록더보기 >> path').first().click(),
+    ]);
+
+    // * : AAPL 상세 페이지로 이동
+    await Promise.all([
+      page.waitForNavigation(/*{ url: 'http://localhost:3000/market/stock/AAPL' }*/),
+      page.locator('text=AAPL').click(),
+    ]);
+    // * : 시장 상세 페이지에서 관심목록에 종목 추가
+    await page.locator('button').nth(1).click();
+    // * : 시장 상세 페이지에서 관심목록에 종목 삭제
+    await page.locator('button').nth(1).click();
+
+    // * : 관심 목록 없음 확인
+    await page.locator('text=시장뉴스관심목록더보기 >> path').nth(2).click();
+    await expect(page).toHaveURL('http://localhost:3000/watchlist');
+
+    const locator = page.locator('.StockList_bl_emptyData__4p4jN');
+    await expect(locator).toContainText('관심 목록이 없습니다.');
+  });
 });
