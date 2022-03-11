bff1e679a24c1f2aac43cff7fa9ad95f39bbad5f
BonhaengLee <bon952014@ajou.ac.kr>
2022-03-11 17:09:40 +0900

    [Refactor] remove unnecessary state

--- components/StockList/index.tsx
+++ components/StockList/index.tsx
@@ -73,6 +73,8 @@ const StockList = ({ editMode }: { editMode?: boolean }): JSX.Element => {
     }
   }, [watchlistData, marketStockData, marketCryptoData]);
 
+  console.log(watchlistData, priceList);
+
   return (
     <>
       {watchlistStatus === 'loading' && <Spinner />}
